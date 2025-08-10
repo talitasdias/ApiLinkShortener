9e39093d28622264039f445ea9dbb9bcef8284db
tdias <dias.talitaa22@gmail.com>
2025-08-10 14:53:59 -0400

    feat:add UrlMappings entity

--- ApiLinkShortener/Database/ApiDbContext.cs
+++ ApiLinkShortener/Database/ApiDbContext.cs
@@ -4,4 +4,12 @@ namespace ApiLinkShortener.Database;
 
 public class ApiDbContext(DbContextOptions<ApiDbContext> options) : DbContext(options)
 {
+    public DbSet<UrlMappings> UrlMappings { get; set; }
+}
+
+public class UrlMappings
+{
+    public int Id { get; set; }
+    public string? ShortenUrl { get; set; }
+    public string? LongUrl { get; set; }
 }
